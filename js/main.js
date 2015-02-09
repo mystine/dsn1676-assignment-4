9d21286f19afc22f0a252a6e717acb85c031eade
Mystine Laframboise Vien <mystinelaframboisevien@Mystines-MacBook-Pro.local>
2015-02-09 18:31:31 -0500

    html + css created

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// alert("Hello");
+
+var firstName = "Mystine"; // Strings: just some text
+console.log(firstName);
+
+var isHuman = true; // Boolean: true or false
+var age = 21;
+
+// Array: a container that holds many things
+var dinos = ['Stegosayrus', 'T-Rex', 'Apayodsutud'];
+console.log(dinos[0]);
+
+//Object: a container that holds manty variables
+var trex = {
+    weight: '3 tonnes',
+    isMeatEater: true,
+    age: 10000000
+};
+
+console.log(trex.age);
